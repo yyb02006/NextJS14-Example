0f0a37f305cb27cdded539c0fe247a27d1681ce0
alwjrqns <yyb02006@gmail.com>
2024-04-02 23:51:33 +0900

    Comment : 앱라우터의 라우팅과 네비게이션 작동원리 추가

--- src/app/linking/page.tsx
+++ src/app/linking/page.tsx
@@ -8,22 +8,60 @@ import { usePathname, useRouter, useSearchParams } from 'next/navigation'
  * Routing => [Linking and Navigating]#<Link> Component
  *
  *
- * 1. App Router의 페이지 이동시 기본 동작은 스크롤을 최상단으로 올리고, 앞으로가기 또는 뒤로가기가 실행되면 스크롤을 유지하는 것이다.
+ * 1. 앱 라우터에서 링크, 네비게이션을 구현하거나, 훅, 컴포넌트 사용 시 알아둘 기능들
  *
- * 2. <Link>컴포넌트의 기본은 옛날과 그리 다르지 않다. href를 필수로 받고, replace, scroll, prefecth옵션이 있다.
+ * - App Router의 페이지 이동시 기본 동작은 스크롤을 최상단으로 올리고, 앞으로가기 또는 뒤로가기가 실행되면 스크롤을 유지하는 것이다.
  *
- * 3. HTML표준에 hash link라는 녀석이 있는데, URL뒤에 #id를 입력하면 페이지 이동시 해당 id를 가진 요소로 스크롤된다.
- *    <Link>컴포넌트가 <a>요소로 렌더링되기 때문에 가능한 동작이고, 이 기능은 nextjs의 공식문서에서도 많이 사용되고 있다.
- *    hash link에 해당하는 id를 가진 요소가 없다면 nextjs는 page로 스크롤시킨다.
+ * - <Link>컴포넌트의 기본은 옛날과 그리 다르지 않다. href를 필수로 받고, replace, scroll, prefecth옵션이 있다.
  *
- * 4. usePathname 훅을 사용할 수 있다. 예전에 useRouter에서 가져오던 pathname만 따로 가져올 수 있게 되었다.
+ * - HTML표준에 hash link라는 녀석이 있는데, URL뒤에 #id를 입력하면 페이지 이동시 해당 id를 가진 요소로 스크롤된다.
+ * - <Link>컴포넌트가 <a>요소로 렌더링되기 때문에 가능한 동작이고, 이 기능은 nextjs의 공식문서에서도 많이 사용되고 있다.
+ * - hash link에 해당하는 id를 가진 요소가 없다면 nextjs는 page로 스크롤시킨다.
  *
- * 5. useSearchParams 훅을 사용할 수 있다. 예전에 useRouter에서 가져오던 query만 따로 가져올 수 있게 되었다.
+ * - usePathname 훅을 사용할 수 있다. 예전에 useRouter에서 가져오던 pathname만 따로 가져올 수 있게 되었다.
  *
- * 6. useRouter 훅을 사용할 수 있다. 페이지 이동을 프로그래밍 방식으로 가능하게 한다. 종종 필요하다.
- *    *기존의 useRouter는 next/router에 있고, 새로운 useRouter는 next/navigation에 있으니 import시 주의하자.
+ * - useSearchParams 훅을 사용할 수 있다. 예전에 useRouter에서 가져오던 query만 따로 가져올 수 있게 되었다.
  *
- * 7. 서버 컴포넌트에서는 useRouter 대신 사용할 수 있는 redirect 함수가 있다. server-linking에서 설명.
+ * - useRouter 훅을 사용할 수 있다. 페이지 이동을 프로그래밍 방식으로 가능하게 한다. 종종 필요하다.
+ * - *기존의 useRouter는 next/router에 있고, 새로운 useRouter는 next/navigation에 있으니 import시 주의하자.
+ *
+ * - 서버 컴포넌트에서는 useRouter 대신 사용할 수 있는 redirect 함수가 있다. server-linking에서 설명.
+ *
+ *
+ * 2. 앱 라우터에서 라우팅의 작동원리
+ *
+ * - Code Splitting : 애플리케이션 코드는 세그먼트별로 코드 분할되어서 각 요청에 대해 제공된다.
+ *                    특히 서버 컴포넌트는 서버에서 렌더링되어 HTML을 생성하고, 브라우저는 이 HTML을 빠르게 렌더링하고 클라이언트 동작을 처리하여 최적화된다.
+ *
+ * - Prefetching : 프리페칭은 <Link> 컴포넌트나 useRouter 훅이 반환하는 객체의 prefetch 메서드를 이용하여 처리된다.
+ *                 <Link> 컴포넌트 사용 시 prefetch 프로퍼티가 true라면 컴포넌트가 뷰포트에 들어갈 때 프리페칭되고, prefetch 메서드를 실행해도 같은 일이 일어난다.
+ *                 정적 라우트에서는 프리페치와 캐시가 완전히 이루어진다.
+ *                 동적 라우트에서는 컴포넌트 트리에서 첫번째 loading 스페셜 파일까지만 이루어지는데, 캐시같은 경우 30초 동안만 유지된다.
+ *
+ * - Caching : Nextjs에는 Router Cache라고 불리는 인-메모리 클라이언트측 캐시가 있다.
+ *             사용자가 앱을 탐색할 때, prefetch된 세그먼트와 서버컴포넌트 페이로드가 이 Router Cache에 저장된다.
+ *             navigation에서는 서버에 데이터를 요청하기 이전에 캐시를 먼저 살핀다.
+ *
+ *             ref : https://nextjs.org/docs/app/building-your-application/caching#router-cache
+ *
+ * - Partial Rendering : Nextjs에 부분 렌더링이 있는데, 이 것은 헤더, 네비게이션 바, 푸터 등 공통 요소는 상위 세그먼트에서 처리하고
+ *                       컨텐츠를 그리는 하위 세그먼트가 경로에 따라서 렌더링되는 것을 말한다. 일부는 정적, 일부는 동적 렌더링.
+ *                       페이지 라우터에서 이런 라우팅 처리를 하는 건 못 봤는데, 신기술인가보다.
+ *
+ *                       ref : https://nextjs.org/docs/app/building-your-application/routing/linking-and-navigating#4-partial-rendering
+ *
+ * - Soft Navigation : 위와 같은 맥락에서 나오는 개념인데, 페이지 이동 시 공유하고 있는 상위 레이아웃은 변하지 않고
+ *                     변경된 세그먼트에 해당하는 부분만 렌더링해서 변경된 세그먼트를 제외하고는 상태를 유지할 수 있다.
+ *                     Partial Rendering, Soft Navigation, Parallel Routes를 관련지어서 살펴보자.
+ *
+ *                     ref(Parallel Routes) : https://nextjs.org/docs/app/building-your-application/routing/parallel-routes
+ *
+ * - Back and Forward Navigation : 앞, 뒤 히스토리 이동시 nextjs에서는 Router Cache에서 먼저 데이터를 꺼내온다.
+ *
+ * - Routing between pages/ and app/ : 페이지 라우터를 앱 라우터로 점진적 마이그레이션 할 때, 페이지 라우트와 앱 라우트가 동시에 존재할 수 있는데
+ *                                     이때 nextjs가 이동을 자연스럽게 관리해줄 수 있지만, 가끔 문제가 생길 수 있다는 듯 하다. 자세한 건 공식문서 참고.
+ *
+ *                                     ref : https://nextjs.org/docs/app/building-your-application/routing/linking-and-navigating#7-routing-between-pages-and-app
  *
  *
  * ref : https://nextjs.org/docs/app/building-your-application/routing/linking-and-navigating#link-component
