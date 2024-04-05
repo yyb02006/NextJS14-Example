c886ef5e3a07eb09da6d8554e2134d68f80638d6
alwjrqns <yyb02006@gmail.com>
2024-04-05 23:43:07 +0900

    Feat : [Example] 서스펜스 컴포넌트 추가
    - FetchPost 컴포넌트를 스트리밍

--- src/app/component-streaming/use-streaming/page.tsx
+++ src/app/component-streaming/use-streaming/page.tsx
@@ -1,3 +1,13 @@
+import FetchPost from '#/components/fetch-post'
+import { Suspense } from 'react'
+
 export default function StreamingComponentsPage() {
-  return <section>Hello StreamingComponentsPage!</section>
+  return (
+    <section>
+      Hello StreamingComponentsPage!
+      <Suspense fallback={<p>loading post...</p>}>
+        <FetchPost />
+      </Suspense>
+    </section>
+  )
 }
