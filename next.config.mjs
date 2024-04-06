fd0d44fc33967a55def0b4840283d44b6315fe8e
alwjrqns <yyb02006@gmail.com>
2024-04-07 07:32:25 +0900

    Feat : next.config.mjs에 리다이렉션 기능 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true }
+const nextConfig = {
+  reactStrictMode: true,
+
+  /**
+   * redirects 옵션으로 path, header, cookie, and query matching에 대해 리다이렉션 가능
+   * ref : https://nextjs.org/docs/app/api-reference/next-config-js/redirects
+   * */
+  async redirects() {
+    return [{ source: '/linking-and-navigating/redirecting', destination: '/', permanent: false }]
+  },
+}
 
 export default nextConfig
