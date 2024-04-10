9f4dc64b84ea23340b25a455d04c4a752c488393
alwjrqns <yyb02006@gmail.com>
2024-04-11 03:00:06 +0900

    Feat : [hello] 라우트에 빌드타임 정적 생성 기능 추가
    - page.tsx에 generateStaticParams 함수 추가 : 다이나믹 라우트 정적 생성 함수
    - page.tsx에 세그먼트 컨픽 추가
    - page.tsx에 fetch모방을 위한 비동기 함수 추가

--- src/app/dynamic-routes/[hello]/page.tsx
+++ src/app/dynamic-routes/[hello]/page.tsx
@@ -1,3 +1,18 @@
+export const dynamicParams = false
+
+const fetchParams = () => {
+  return new Promise<{ success: boolean; params: string[] }>((resolve) => {
+    setTimeout(() => {
+      resolve({ success: true, params: ['n', 'e', 'x', 't'] })
+    }, 3000)
+  })
+}
+
+export async function generateStaticParams() {
+  const response = await fetchParams()
+  return response.params.map((param) => ({ hello: param }))
+}
+
 export default function DynamicHelloPage({ params }: { params: { hello: string } }) {
   return (
     <div>
