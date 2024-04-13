c64352b303a4e51b1baa85aeb187d5bec0241cb3
alwjrqns <yyb02006@gmail.com>
2024-04-14 01:59:11 +0900

    Feat : parallel-routes 라우트 layout.tsx 스타일 수정
    - root가 defalut파일을 렌더링할 시 어두운 색이 되도록 수정
    - root에 대해서도 활성 세그먼트를 추적할 수 있도록 훅 추가

--- src/app/parallel-routes/layout.tsx
+++ src/app/parallel-routes/layout.tsx
@@ -26,10 +26,13 @@ export default function StreamingCommmonLayout({
   hello: ReactNode
   world: ReactNode
 }) {
+  console.log(world)
+  console.log(hello)
   const helloSlot = useSelectedLayoutSegment('hello')
   const worldSlot = useSelectedLayoutSegment('world')
+  const rootChildren = useSelectedLayoutSegment()
   const selecteds = useSelectedLayoutSegments('hello')
-  console.log(helloSlot, worldSlot, selecteds)
+  console.log('hello = ' + helloSlot, ',world = ' + worldSlot, ',root = ' + rootChildren, selecteds)
   const navButtons = [
     { label: 'to Hello', path: '/parallel-routes/hello' },
     { label: 'to World', path: '/parallel-routes/world' },
@@ -52,23 +55,19 @@ export default function StreamingCommmonLayout({
       </nav>
       <section className="flex h-full w-full items-center justify-center pt-16">
         <div className="gire grid h-[600px] w-[1200px] grid-cols-2 gap-4 text-2xl font-bold ">
-          <div className="flex h-full w-full items-center justify-center bg-pink-500">
+          <div
+            className={`flex h-full w-full items-center justify-center ${rootChildren === '__DEFAULT__' ? 'bg-pink-800' : 'bg-pink-500'}`}
+          >
             {children}
           </div>
           <div className="flex w-full flex-col gap-4">
             <div
-              className={
-                'flex h-full items-center justify-center ' +
-                `${helloSlot === '__DEFAULT__' ? 'bg-indigo-800' : 'bg-indigo-500'}`
-              }
+              className={`flex h-full items-center justify-center ${helloSlot === '__DEFAULT__' ? 'bg-indigo-800' : 'bg-indigo-500'}`}
             >
               {hello}
             </div>
             <div
-              className={
-                'flex h-full items-center justify-center ' +
-                `${worldSlot === '__DEFAULT__' ? 'bg-sky-800' : 'bg-sky-600'}`
-              }
+              className={`flex h-full items-center justify-center ${worldSlot === '__DEFAULT__' ? 'bg-sky-800' : 'bg-sky-600'}`}
             >
               {world}
             </div>
