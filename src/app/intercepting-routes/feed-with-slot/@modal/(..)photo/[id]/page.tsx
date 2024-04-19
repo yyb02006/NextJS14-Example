2e42b8fe88f51de246ef4c6f14cee6155d3122d3
alwjrqns <yyb02006@gmail.com>
2024-04-20 00:39:33 +0900

    Feat : feed-with-slot 라우트 Modal에 닫기 버튼 추가
    - 모달의 page.tsx를 클라이언트 컴포넌트로 변경

--- src/app/intercepting-routes/feed-with-slot/@modal/(..)photo/[id]/page.tsx
+++ src/app/intercepting-routes/feed-with-slot/@modal/(..)photo/[id]/page.tsx
@@ -1,9 +1,25 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
 export default function PhotoModal({ params }: { params: { id: string } }) {
+  const router = useRouter()
   return (
     <article className="fixed left-0 top-0 flex h-screen w-screen items-center justify-center">
       <div className="absolute left-0 top-0 h-full w-full bg-black opacity-50"></div>
-      <div className="relative flex h-[600px] w-[1000px] items-center justify-center bg-red-500 text-3xl font-semibold">
-        this modal about photo{params.id} page
+      <div className="relative h-[600px] w-[1000px] bg-red-500">
+        <div className="flex h-full w-full items-center justify-center text-3xl font-semibold">
+          this modal about photo{params.id} page
+        </div>
+        <button
+          onClick={() => {
+            router.back()
+          }}
+          className="absolute right-2 top-2 flex h-8 w-8 items-center"
+        >
+          <div className="absolute left-0 top-[50%] h-[2px] w-full rotate-45 bg-black" />
+          <div className="absolute left-0 top-[50%] h-[2px] w-full rotate-[135deg] bg-black" />
+        </button>
       </div>
     </article>
   )
