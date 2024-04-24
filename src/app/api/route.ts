8ed28e692c5afa41645655067b770e7fc55e6a6d
alwjrqns <yyb02006@gmail.com>
2024-04-24 23:49:34 +0900

    Feat : api/route.ts API 라우트 추가
    - 기본적인 라우트 예시 추가

---
+++ src/app/api/route.ts
@@ -0,0 +1,15 @@
+const fetchDogData = () => {
+  return new Promise<{ success: boolean; data: string }>((resolve) => {
+    setTimeout(() => {
+      resolve({
+        success: true,
+        data: 'The dog is a domesticated descendant of the wolf.',
+      })
+    }, 3000)
+  })
+}
+
+export default async function GET() {
+  const { data, success } = await fetchDogData()
+  return Response.json({ data })
+}
