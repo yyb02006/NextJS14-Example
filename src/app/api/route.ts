afc23cf85e49ca48624bf4cae7691e22d0bf3412
alwjrqns <yyb02006@gmail.com>
2024-04-26 23:38:06 +0900

    Feat : api API 라우트 route.ts Revalidation 옵션 추가
    - fetch 함수와 config 양 쪽에 추가

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,5 @@
+export const revalidate = 60
+
 const fetchDogData = () => {
   return new Promise<{ success: boolean; data: string }>((resolve) => {
     setTimeout(() => {
@@ -10,6 +12,15 @@ const fetchDogData = () => {
 }
 
 export async function GET() {
+  /* 
+  fetch 함수에 아래처럼 next revalidation 옵션 추가 가능
+
+  const respones = await (
+    await fetch(
+      'https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1',
+      { next: { revalidate: 60 } },
+    )
+  ).json() */
   const { data, success } = await fetchDogData()
   return Response.json({ data })
 }
