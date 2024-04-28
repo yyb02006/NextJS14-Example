beca61ef177e6974d3f997bb05dfc051fcc92b5c
alwjrqns <yyb02006@gmail.com>
2024-04-28 23:23:00 +0900

    Feat : api API 라우트 route.ts POST 핸들러 추가
    - API라우트에서의 리다이렉트 함수 사용 예시 추가
    - fetchDogData 함수에 success 파라미터 추가

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,13 +1,14 @@
 import { cookies, headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 import { NextRequest } from 'next/server'
 
 export const revalidate = 60
 
-const fetchDogData = () => {
+const fetchDogData = (success: boolean = true) => {
   return new Promise<{ success: boolean; data: string }>((resolve) => {
     setTimeout(() => {
       resolve({
-        success: true,
+        success,
         data: 'The dog is a domesticated descendant of the wolf.',
       })
     }, 3000)
@@ -23,12 +24,20 @@ export async function GET(request: NextRequest) {
       'https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1',
       { next: { revalidate: 60 } },
     )
-  ).json() */
+  ).json()
+  */
   const { data, success } = await fetchDogData()
   const cookieStore = cookies()
   const user = cookieStore.get('user')
   const headerList = headers()
   const referer = headerList.get('referer')
   const headersFromRequest = new Headers(request.headers)
+  return Response.json({ data })
+}
+
+export async function POST(request: NextRequest) {
+  const body: { success: boolean } = await request.json()
+  const { data, success } = await fetchDogData(body.success)
+  if (!success || !body.success) redirect('/')
   return Response.json({ data })
 }
