8db0027e564d3a48d91e198ebb239c87a6d1deb8
alwjrqns <yyb02006@gmail.com>
2024-05-04 23:55:09 +0900

    Comment : api API 라우트 route.ts에 라우트 핸들러에 대한 설명 추가

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -20,6 +20,28 @@ const fetchDogData = (success: boolean = true) => {
   })
 }
 
+/**
+ *
+ * Routing => [Route Handlers]
+ *
+ *
+ * 1. 앱라우터에서 라우트 핸들러는 api폴더 아래에 route.ts 파일을 만들어서 사용할 수 있다.
+ *
+ * 2. 각 HTTP 메서드에 대응하는 핸들러를 만들기 위해 GET, POST, PUT, PATCH, DELETE, HEAD, OPTION 함수를 정의해야한다.
+ *    대응하는 메서드를 이름으로 가진 함수가 없다면 405 Method Not ALlowed 응답을 리턴하게 된다.
+ *
+ * 3. 라우트 핸들러에서는 기존 request, response API를 확장한 NextRequest, NextRespones를 사용한다.
+ *    api/next-req-res API 라우트에서 구체적으로 설명.
+ *
+ * 4. GET을 사용할 때는 기본적으로 캐싱을 사용한다.
+ *    다른 HTTP 메서드를 사용하거나, 핸들러 내에서 Request 오브젝트를 사용하거나, cookies, headers와 같은 Dynamic Functions를 사용하는 경우 캐싱이 중지된다.
+ *    Segment Config Options의 dynamic 옵션을 사용해도 캐싱은 중지된다.
+ *    api/caching API 라우트에서 구체적으로 설명.
+ *
+ *
+ * ref : https://nextjs.org/docs/app/api-reference/functions/next-request
+ *
+ * */
 export async function GET(request: NextRequest) {
   /* 
   fetch 함수에 아래처럼 next revalidation 옵션 추가 가능
