fcaa01439ee9a4acc02004c6db3a6fe925b0a416
alwjrqns <yyb02006@gmail.com>
2024-05-10 17:59:16 +0900

    Feat : FetchingFromServer 컴포넌트가 내부 api에 접근하지 않도록 수정

--- src/app/route-handler-request/FetchingFromServer.tsx
+++ src/app/route-handler-request/FetchingFromServer.tsx
@@ -1,4 +1,13 @@
+const fetchDate = () => {
+  return new Promise<{ time: number }>((resolve) => {
+    setTimeout(() => {
+      resolve({ time: new Date().getTime() })
+    }, 300)
+  })
+}
+
 export default async function ServerComponentFetch() {
-  const data = await (await fetch('http://localhost:3000/api/caching')).json()
+  const data = await fetchDate()
+  console.log('run server')
   return <div>{data.time}</div>
 }
