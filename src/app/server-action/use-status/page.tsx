255921dfdefd1322756a9183e3440c43616fa896
alwjrqns <yyb02006@gmail.com>
2024-05-23 23:57:55 +0900

    Comment : use-status 라우트 주석 추가
    - useFormStatus 훅을 사용한 Pending 상태 지원에 대한 예시 추가

--- src/app/server-action/use-status/page.tsx
+++ src/app/server-action/use-status/page.tsx
@@ -1,5 +1,17 @@
 import { SubmitButton } from '#/components/Buttons'
 
+/**
+ *
+ * Data Fetching => [Server Actions and Mutations]#Pending States
+ *
+ *
+ * 1. useFormStatus 훅을 사용해 Pending 상태를 따로 구현하지 않고 사용할 수 있다.
+ *    Buttons 컴포넌트 참고
+ *
+ *
+ * ref : https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions-and-mutations#pending-states
+ *
+ */
 export default function UseStatusPage() {
   async function createInvoice(formData: FormData) {
     'use server'
