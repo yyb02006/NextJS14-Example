033c6b51df558e7f5acd85aa4dbc1dd793ecfe22
alwjrqns <yyb02006@gmail.com>
2024-05-28 23:43:21 +0900

    Comment : use-optimistic 라우트 page.tsx 설명 수정
    - 긍정적 업데이트 => 낙관적 업데이트 수정

--- src/app/server-action/use-optimistic/page.tsx
+++ src/app/server-action/use-optimistic/page.tsx
@@ -12,11 +12,9 @@ type Message = {
  *
  *
  * 1. Optimistic Update란 서버에서 응답하기 전에 예상할 수 있는 UI를 렌더링하는 것을 말한다.
- *    SNS에서 좋아요 버튼을 클릭할 때, 응답을 기다리지 않고 아이콘이 활성화되는 것이 바로 긍정적 업데이트이다.
+ *    SNS에서 좋아요 버튼을 클릭할 때, 응답을 기다리지 않고 아이콘이 활성화되는 것이 바로 낙관적 업데이트이다.
  *
  * 2. 리액트에서는 Optimistic Update의 로직을 추상화 하여 사용할 수 있도록 useOptimistic 훅을 제공하고 있다.
- *
- * 3.
  *
  *
  * ref : https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions-and-mutations#optimistic-updates
