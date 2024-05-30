101531e854f347149580ad35ed60c24fca91c9d8
alwjrqns <yyb02006@gmail.com>
2024-05-30 23:58:18 +0900

    Feat : caching 라우트 추가

---
+++ src/app/caching/page.tsx
@@ -0,0 +1,10 @@
+export default function Caching() {
+  return (
+    <ul>
+      <li>Request Memoization</li>
+      <li>Data Cache</li>
+      <li>Router Cache</li>
+      <li>Full Route Cache</li>
+    </ul>
+  )
+}
