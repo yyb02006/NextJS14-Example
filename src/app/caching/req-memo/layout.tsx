0d74bca8ad6cef13f417891c3875f555f53bcf4f
alwjrqns <yyb02006@gmail.com>
2024-06-09 00:03:16 +0900

    Feat : req-memo 라우트 layout.tsx의 ui 변경
    - 이미지가 좌우로 배열될 수 있도록 변경

--- src/app/caching/req-memo/layout.tsx
+++ src/app/caching/req-memo/layout.tsx
@@ -1,16 +1,22 @@
 import Image from 'next/image'
 import { ReactNode } from 'react'
+import ClientFetchComponent from './ClientCatComponent'
 
 export default async function TestReq({ children }: { children: ReactNode }) {
   const data = await (await fetch('https://api.thecatapi.com/v1/images/search')).json()
   const url = data[0].url
   return (
-    <section>
-      <div className="relative h-[225px] w-[400px]">
-        <Image alt="altImg" src={url} layout="fill" objectFit="cover" />
+    <section className="p-10">
+      <div className="flex">
+        <div>
+          <div className="relative h-[225px] w-[400px]">
+            <Image alt="altImg" src={url} layout="fill" objectFit="cover" />
+          </div>
+          <span className="text-rose-400">Layout Fetch Image</span>
+        </div>
+        {children}
       </div>
-      <span className="text-rose-400">Layout Fetch Image</span>
-      {children}
+      <ClientFetchComponent />
     </section>
   )
 }
