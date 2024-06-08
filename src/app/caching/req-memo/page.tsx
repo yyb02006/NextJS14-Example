1ee6922c50a9f31043c9f18e561e444c528c3c75
alwjrqns <yyb02006@gmail.com>
2024-06-09 00:02:02 +0900

    Feat : req-memo 라우트 page.tsx에 cat image를 fetching하는 컴포넌트 추가

--- src/app/caching/req-memo/page.tsx
+++ src/app/caching/req-memo/page.tsx
@@ -7,16 +7,35 @@ interface DogData {
   height: number
 }
 
-export default async function ReqMemoPage() {
+const fetchCatImage = async () => {
   const data: DogData[] = await (await fetch('https://api.thecatapi.com/v1/images/search')).json()
   const url = data[0].url
-  console.log('url = ', data[0].url)
+  return url
+}
+
+const CatImage = async () => {
+  const url = await fetchCatImage()
   return (
-    <section>
+    <div>
       <div className="relative h-[225px] w-[400px]">
         <Image alt="altImg" src={url} layout="fill" objectFit="cover" />
       </div>
-      <span className="text-rose-400">Page Fetch Image</span>
+      <span className="text-rose-400">Server Component Fetch Image</span>
+    </div>
+  )
+}
+
+export default async function ReqMemoPage() {
+  const url = await fetchCatImage()
+  return (
+    <section className="flex">
+      <div>
+        <div className="relative h-[225px] w-[400px]">
+          <Image alt="altImg" src={url} layout="fill" objectFit="cover" />
+        </div>
+        <span className="text-rose-400">Page Fetch Image</span>
+      </div>
+      <CatImage />
     </section>
   )
 }
