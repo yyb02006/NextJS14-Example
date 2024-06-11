24997f7462e8a02413de89f7b9c0937788b9f96b
alwjrqns <yyb02006@gmail.com>
2024-06-11 23:54:28 +0900

    Feat : req-memo 라우트 CatImage 컴포넌트 주석처리및 빌드시 특이 사항 주석 추가

--- src/app/caching/req-memo/page.tsx
+++ src/app/caching/req-memo/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import CatImage from './ServerCatComponent'
 
 interface DogData {
   id: string
@@ -7,29 +8,36 @@ interface DogData {
   height: number
 }
 
+// no-store 캐시 상태에서는 빌드타임 html도 생성되지 않음
 const fetchCatImage = async () => {
   const data: DogData[] = await (await fetch('https://api.thecatapi.com/v1/images/search')).json()
   const url = data[0].url
+  console.log('run fetch in page')
   return url
 }
 
-const CatImage = async () => {
+// Image 컴포넌트가 있는 경우 Component Call 로그가 출력되지 않는 현상 발견
+// no-store fetch일 경우 Component Call 로그는 출력되지 않고, default fecth일 경우 모든 Call 로그가 두 번 출력되는 현상 발견
+// catImage 컴포넌트를 모듈로 사용하고, fetchCatImage 함수를 no-store로 사용하면 모든 Call 로그가 한 번 출려되는 현상 발견
+/* const CatImage = async () => {
   const url = await fetchCatImage()
+  console.log('Component call = ', url)
   return (
-    <div>
+    <div className="border border-sky-600">
       <div className="relative h-[225px] w-[400px]">
         <Image alt="altImg" src={url} layout="fill" objectFit="cover" />
       </div>
       <span className="text-rose-400">Server Component Fetch Image</span>
     </div>
   )
-}
+} */
 
 export default async function ReqMemoPage() {
   const url = await fetchCatImage()
+  console.log('Page call = ', url)
   return (
     <section className="flex">
-      <div>
+      <div className="border border-sky-600">
         <div className="relative h-[225px] w-[400px]">
           <Image alt="altImg" src={url} layout="fill" objectFit="cover" />
         </div>
