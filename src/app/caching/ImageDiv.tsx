28596f88023e1825b96121313badbaedae7614fa
alwjrqns <yyb02006@gmail.com>
2024-06-15 23:46:15 +0900

    Refactor : ImageDiv.tsx 컴포넌트에서 레거시 props 제거

--- src/app/caching/ImageDiv.tsx
+++ src/app/caching/ImageDiv.tsx
@@ -4,11 +4,7 @@ export const ImageDiv = ({ url, description }: { url: string; description: strin
   return (
     <div className="border border-sky-600">
       <div className="relative flex h-[225px] w-[400px] items-center justify-center">
-        {url ? (
-          <Image alt="altImg" src={url} layout="fill" objectFit="cover" />
-        ) : (
-          'loading image...'
-        )}
+        {url ? <Image alt="altImg" src={url} className="object-cover" fill /> : 'loading image...'}
       </div>
       <span className="text-rose-400">{description}</span>
     </div>
