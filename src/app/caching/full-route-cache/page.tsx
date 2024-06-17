9717ee971fd004fa34600275973cfdf0cf6f1c3f
alwjrqns <yyb02006@gmail.com>
2024-06-18 05:31:14 +0900

    Feat : full-route-cache 라우트 추가

---
+++ src/app/caching/full-route-cache/page.tsx
@@ -0,0 +1,14 @@
+export default function FullRouteCachePage() {
+  console.log('Full-Route-Cached Page')
+  return (
+    <section>
+      Hello FullRouteCachePage!
+      <ul>
+        <li>Un-Ordered List 1</li>
+        <li>Un-Ordered List 2</li>
+        <li>Un-Ordered List 3</li>
+        <li>Un-Ordered List 4</li>
+      </ul>
+    </section>
+  )
+}
