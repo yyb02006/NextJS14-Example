b7a9b50142e6b3d15e78f0bd4e66698253911f53
alwjrqns <yyb02006@gmail.com>
2024-06-19 23:59:30 +0900

    Comment : full-route-cache/dynamic-route 라우트 주석 추가

--- src/app/caching/full-route-cache/dynamic-route/page.tsx
+++ src/app/caching/full-route-cache/dynamic-route/page.tsx
@@ -1,5 +1,14 @@
 export const dynamic = 'force-dynamic'
 
+/**
+ *
+ * Caching => #Full Route Cache
+ *
+ *
+ * 1. 다이나믹 라우트, API핸들러의 경우에는 Full Route Cache가 Optout된다.
+ *
+ *
+ */
 export default function DynamicRoute() {
   console.log('Full-Route-Cached Optout Page')
   return <div>Hello DynamicRoutePage!</div>
