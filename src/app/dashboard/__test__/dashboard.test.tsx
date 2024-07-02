34ac940cac3dd30919b8a3da8ac307385d8e8eb9
alwjrqns <yyb02006@gmail.com>
2024-07-02 23:59:15 +0900

    Test : dashboard 라우트에 대한 테스트 추가
    - 페이지 컴포넌트에 대한 렌더링 테스트 추가

--- src/app/dashboard/__test__/dashboard.test.tsx
+++ src/app/dashboard/__test__/dashboard.test.tsx
@@ -1,8 +1,7 @@
 import '@testing-library/jest-dom'
-import { getByRole, getByText, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import DashboardPage from '../page'
 import DashboardLayout from '../layout'
-import TestPage from '../testPage'
 
 describe('Layout Component Rendering Test', () => {
   it('Hello DashBoardLayout text를 가진 heading이 있어야 한다.', () => {
@@ -10,9 +9,16 @@ describe('Layout Component Rendering Test', () => {
     const headingEl = screen.getByRole('heading', { level: 1, name: 'Hello DashBoardLayout' })
     expect(headingEl).toBeInTheDocument()
   })
-  it('children 프로퍼티에 div를 전달된 요소가 있어야한다', () => {
+  it('children 프로퍼티에 전달된 children 텍스트를 가진 요소가 있어야한다', () => {
     render(<DashboardLayout children={<div>children</div>} />)
     const headingEl = screen.getByText('children')
+    expect(headingEl).toBeInTheDocument()
+  })
+})
+describe('Page Component Rendering Test', () => {
+  it('Hello, DashBoard Page! text를 가진 div가 있어야 한다.', () => {
+    render(<DashboardPage />)
+    const headingEl = screen.getByText('Hello, Dashboard Page!')
     expect(headingEl).toBeInTheDocument()
   })
 })
