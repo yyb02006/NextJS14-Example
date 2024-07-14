f39e10838cac9793f0b8f76d9d06c67b964dd89e
alwjrqns <yyb02006@gmail.com>
2024-07-15 00:51:39 +0900

    Test: redirect.test.tsx 테스트 수정
    - describe를 의미있게 분기
    - 쓸데없는(ㅎㅎ) 주석 삭제
    - 중복코드 함수로 분리

--- src/app/linking-and-navigating/redirecting/redirect/redirect.test.tsx
+++ src/app/linking-and-navigating/redirecting/redirect/redirect.test.tsx
@@ -1,43 +1,68 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import ServerRedirecting from './page'
 import { timeout } from '#/utils/timeout'
 import { redirect } from 'next/navigation'
 
-// async server component redirect에 대해 제대로된 테스트가 불가.
-// 서버 환경에서는 redirect 모킹도 안되고 redirect 실행도 안되고 그냥 씨발
 jest.mock('#/utils/timeout')
 jest.mock('next/navigation', () => ({ redirect: jest.fn() }))
+const mockTimeout = timeout as jest.Mock
 
-describe('ServerRedirecting 컴포넌트', () => {
-  const role = 'admin'
-  const mockTimeout = timeout as jest.Mock
-  it('api 응답의 success 프로퍼티가 false일 경우 redirect 함수가 호출되어야 한다', async () => {
-    // given
-    mockTimeout.mockReturnValue({ success: false })
+const renderPageWithMock = async ({
+  mockTimeoutValue,
+}: {
+  mockTimeoutValue: { success: boolean; role?: 'admin' | 'user' }
+}) => {
+  mockTimeout.mockResolvedValue(mockTimeoutValue)
+  const Page = await ServerRedirecting()
+  render(Page)
+}
 
-    // when
-    const Page = await ServerRedirecting()
-    render(Page)
+describe('api 응답 실패', () => {
+  it('redirect 함수가 호출되어야 한다', async () => {
+    // Given
+    await renderPageWithMock({ mockTimeoutValue: { success: false } })
 
-    // then
+    // When
+
+    // Then
     expect(redirect).toHaveBeenCalledWith('/linking-and-navigating')
   })
-  it(`api 응답의 success 프로퍼티가 true일 경우 'Hello ServerRedirecting Page!'텍스트가 있는 div요소가 렌더링 되어야 한다`, async () => {
-    // given
-    mockTimeout.mockReturnValue({ success: true, role })
+})
 
-    // when
-    const Page = await ServerRedirecting()
-    render(Page)
+describe('api 응답 성공', () => {
+  it('redirect 함수가 호출되지 않아야 한다', async () => {
+    // Given
+    await renderPageWithMock({ mockTimeoutValue: { success: true } })
 
-    // then
+    // When
+
+    // Then
+    expect(redirect).not.toHaveBeenCalled()
+  })
+
+  it(`'Hello ServerRedirecting Page!'텍스트가 있는 h1요소가 있어야 한다`, async () => {
+    // Given
+    await renderPageWithMock({ mockTimeoutValue: { success: true, role: 'admin' } })
+
+    // When
+
+    // Then
     const headingEl = screen.getByRole('heading', {
       name: 'Hello ServerRedirecting Page!',
       level: 1,
     })
-    const textEl = screen.getByText(`your role is ${role}`, { selector: 'div' })
-    expect(redirect).not.toHaveBeenCalled()
     expect(headingEl).toBeInTheDocument()
+  })
+
+  it(`'your role is admin'텍스트가 있는 div요소가 있어야 한다`, async () => {
+    // Given
+    const role = 'admin'
+    await renderPageWithMock({ mockTimeoutValue: { success: true, role } })
+
+    // When
+
+    // Then
+    const textEl = screen.getByText(`your role is ${role}`, { selector: 'div' })
     expect(textEl).toBeInTheDocument()
   })
 })
