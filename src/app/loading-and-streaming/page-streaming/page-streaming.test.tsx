f106b10138abc23a7fbbcf6739bd4c52e2f142b7
alwjrqns <yyb02006@gmail.com>
2024-07-18 23:44:07 +0900

    Test : page-streaming.test.tsx 테스트 수정
    - 타이머 함수를 바로 완료시킬 수 있도록 useFakeTimer 메서드 추가
    - 텍스트 노드 분절 이슈를 해결하기 위해 getAllTextContent 함수 추가

--- src/app/loading-and-streaming/page-streaming/page-streaming.test.tsx
+++ src/app/loading-and-streaming/page-streaming/page-streaming.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen, waitFor } from '@testing-library/react'
 import ChildComponent from './ChildComponent'
 
-// timeout function 모킹해서 5000 timeout error 관리하기
 // children으로 직접 자식 컴포넌트를 받는 테스트 가랑이 사이로 들어가도록 수정
+
+function getAllTextContent(element: Element) {
+  return Array.from(element.childNodes)
+    .map((node) => node.textContent)
+    .join('')
+}
 
 test(`testCases 객체 배열의 정보에 부합하는 list 요소가 있어야 한다`, async () => {
   // Given
@@ -20,9 +25,14 @@ test(`testCases 객체 배열의 정보에 부합하는 list 요소가 있어야
 
   // Then
   testCases.forEach(({ color, delaySec, expectedClass }) => {
-    const textEl =
-      screen.getByText(`${color[0].toUpperCase() + color.slice(1)}Div was fetched successfully. ( delayed ${delaySec}
-      seconds )`)
+    const textEl = screen.getByText((_, element) => {
+      if (!element) return false
+      const textContent = getAllTextContent(element)
+      return (
+        textContent ===
+        `${color[0].toUpperCase() + color.slice(1)}Div was fetched successfully. ( delayed ${delaySec}seconds )`
+      )
+    })
     expect(textEl).toHaveClass(expectedClass)
   })
 })
