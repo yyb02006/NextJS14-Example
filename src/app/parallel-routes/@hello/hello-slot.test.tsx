e445c4a7803b37450216053849d67eb42b372ef0
alwjrqns <yyb02006@gmail.com>
2024-07-22 01:59:57 +0900

    Test : hello-slot.test.tsx 수정
    - 데이터 가져오기 완료 이전 렌더링 테스트 추가
    - HelloSlotDefault 컴포넌트에 대한 테스트 추가

--- src/app/parallel-routes/@hello/hello-slot.test.tsx
+++ src/app/parallel-routes/@hello/hello-slot.test.tsx
@@ -1,22 +1,59 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import HelloSlotPage from './page'
+import HelloSlotDefault from './default'
 
 jest.mock('#/utils/timeout', () => {
   return {
-    timeout: jest.fn(() => ({
-      comment: 'Hello was fetched successfully. ( delayed 3 seconds )',
-    })),
+    timeout: jest.fn(() =>
+      Promise.resolve({ comment: 'Hello was fetched successfully. ( delayed 3 seconds )' }),
+    ),
   }
 })
 
+describe('HelloSlotDefault 테스트', () => {
+  it(`'/@hello/default.tsx' 텍스트가 있어야 한다`, () => {
+    // Given
+    render(<HelloSlotDefault />)
+
+    // When
+
+    // Then
+    expect(screen.getByText('/@hello/default.tsx')).toBeInTheDocument()
+  })
+})
+
 describe('HelloSlotPage 테스트', () => {
-  it(`'Global Nav in Parent Segment Layout'텍스트를 가진 div 요소가 있어야 한다`, async () => {
-    render(<HelloSlotPage />)
-    const textEl = await screen.findByText(
-      'Hello was fetched successfully. ( delayed 3 seconds )',
-      undefined,
-      { timeout: 4000 },
-    )
-    expect(textEl).toBeInTheDocument()
+  describe('초기 상태', () => {
+    it(`'wating for fetching...' 텍스트가 있어야 한다`, async () => {
+      // Given
+      render(<HelloSlotPage />)
+
+      // When
+
+      // Then
+      await waitFor(
+        () => {
+          expect(screen.getByText('wating for fetching...')).toBeInTheDocument()
+        },
+        { timeout: 0 },
+      )
+    })
+  })
+
+  describe('데이터 가져오기 후', () => {
+    it(`'Hello was fetched successfully. ( delayed 3 seconds )' 텍스트가 있어야 한다`, async () => {
+      // Given
+      render(<HelloSlotPage />)
+
+      // When
+      const textEl = await screen.findByText(
+        'Hello was fetched successfully. ( delayed 3 seconds )',
+        undefined,
+        { timeout: 4000 },
+      )
+
+      // Then
+      expect(textEl).toBeInTheDocument()
+    })
   })
 })
