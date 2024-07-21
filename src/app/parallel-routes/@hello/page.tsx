75237dc91b855b007ac73831ddc1ed88e1234906
alwjrqns <yyb02006@gmail.com>
2024-07-21 23:57:40 +0900

    Refactor : FetchComment 컴포넌트를 timeout함수로 대체, 컴포넌트 이름 변경

--- src/app/parallel-routes/@hello/page.tsx
+++ src/app/parallel-routes/@hello/page.tsx
@@ -1,23 +1,16 @@
 'use client'
 
+import { timeout } from '#/utils/timeout'
 import { useEffect, useState } from 'react'
 
-const fetchComment = () => {
-  return new Promise<{ success: boolean; comment: string }>((resolve) => {
-    setTimeout(() => {
-      resolve({
-        success: true,
-        comment: 'Hello was fetched successfully. ( delayed 3 seconds )',
-      })
-    }, 3000)
-  })
-}
-
-export default function ParallelHelloPage() {
+export default function HelloSlotPage() {
   const [data, setData] = useState<string>('')
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetchComment()
+      const response = await timeout({
+        success: true,
+        resolveProps: { comment: 'Hello was fetched successfully. ( delayed 3 seconds )' },
+      })
       console.log(response.comment)
       setData(response.comment)
     }
