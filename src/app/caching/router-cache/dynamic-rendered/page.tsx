aa769efe9f8c64bd495e8e833787fec0480059dd
alwjrqns <yyb02006@gmail.com>
2024-07-24 03:30:43 +0900

    Refactor : 섹션 태그에 h1 태그 추가, 주석이 명확해지도록 수정

--- src/app/caching/router-cache/dynamic-rendered/page.tsx
+++ src/app/caching/router-cache/dynamic-rendered/page.tsx
@@ -6,7 +6,7 @@ export const dynamic = 'force-dynamic'
  *
  * force-dynamic 옵션으로 다이나믹 렌더링되어 서버가 매번 fetch를 실행해야 하지만,
  * Router-Cache에 의해 브라우저에서 일정 시간동안 캐싱되기 때문에 그 시간동안 서버측 fetch 요청은 일어나지 않는다.
- * 이 페이지는 prefetch방식이 true이므로 5분.
+ * 이 페이지는 링크버튼이 있었던 상위레이아웃 파일에서의 prefetch방식이 true이므로 5분.
  *
  * */
 export default async function PreFetchingPage() {
@@ -15,7 +15,7 @@ export default async function PreFetchingPage() {
   console.log('this image was fetched on server')
   return (
     <section>
-      Hello Dynamic-Rendered Page!
+      <h1>Hello Dynamic-Rendered Page!</h1>
       <div className="flex">
         <ImageDiv url={url} description="this page invalidating 5 minutes later" />
       </div>
