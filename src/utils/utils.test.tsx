a661afd1b831f25b9706e2c06f32c1f0357f4180
alwjrqns <yyb02006@gmail.com>
2024-07-31 23:19:55 +0900

    Test : 유틸 함수 테스트 수정
    - useFakeTimer를 실제로 사용하도록 수정
    - resolveProps 반환에 대한 테스트 추가

--- src/utils/utils.test.tsx
+++ src/utils/utils.test.tsx
@@ -1,4 +1,3 @@
-import { render, screen } from '@testing-library/react'
 import { timeout } from './timeout'
 
 describe('timeout 테스트', () => {
@@ -9,13 +8,32 @@ describe('timeout 테스트', () => {
     jest.useRealTimers()
   })
 
-  it('프로미스가 완료된 후 응답객체의 success 프로퍼티가 true여야한다 ', async () => {
+  it('프로미스가 완료된 후 응답객체의 success 프로퍼티가 true여야 한다 ', async () => {
     // Given
-    const result = await timeout({ success: true, resolveProps: { purpose: 'test' } })
+    const response = timeout({ success: true, resolveProps: {} })
 
     // When
+    jest.runAllTimers()
+    const data = await response
 
     // Then
-    expect(result.success).toBe(true)
+    expect(data).toHaveProperty('success', true)
+  })
+
+  it(`인수로 전달된 testCases의 요소들이 'animal' 프로퍼티로 반환되어야 한다`, async () => {
+    // Given
+    const testCases = ['dog', 'cat', 'elephant', 'rat']
+    const response = Promise.all(
+      testCases.map((testCase) => timeout({ success: true, resolveProps: { animal: testCase } })),
+    )
+
+    // When
+    jest.runAllTimers()
+    const data = await response
+
+    // Then
+    testCases.forEach((testCase, idx) => {
+      expect(testCase).toEqual(data[idx].animal)
+    })
   })
 })
